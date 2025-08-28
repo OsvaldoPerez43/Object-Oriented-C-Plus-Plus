7bc4fc231596d8126482683aa3165098917c880c
OsvaldoPerez43 <98354676+OsvaldoPerez43@users.noreply.github.com>
2025-08-28 12:48:29 -0700

    new

---
+++ Lab-1/Hello.cpp
@@ -0,0 +1,15 @@
+/***
+ * In this lab we had to create code that will print Hello World and we had to
+ * learn the directory codes in what they would do. We have to type compile the file
+ * that we have and run the new executable file that we have.
+ ***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout <<"Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
